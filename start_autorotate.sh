7dd05ad5ef50aa3539b6b6900a3986e11fbaaba7
Alberto Pianon <alberto@pianon.eu>
2016-05-05 09:36:20 +0200

    start_autorotate.sh is already started with root UID

--- start_autorotate.sh
+++ start_autorotate.sh
@@ -8,19 +8,6 @@ function zenityNotification
    kill $mypid
 }
 
-
-# check if this script has already been run
-isModuleLoaded=`lsmod | grep hid_sensor_hub`
-if [[ -n $isModuleLoaded ]]; then
-   # check if autorotate.sh is already running, otherwise start it
-   isAutorotateRunning=`ps -A | grep autorotate.sh`
-   if [ -z $isAutorotateRunning ]; then
-      su root -c "autorotate.sh >/tmp/autorotate.log 2>&1 &"
-   fi
-   # this script has already been run, nothing to do here
-   exit 0
-fi
-
 # make screen accessible from root
 export DISPLAY=":0.0"
 xhost local:root
@@ -31,6 +18,19 @@ mkdir $tmpdir
 echo "Autorotate" > $tmpdir/screen_orientation
 echo "normal" > $tmpdir/screen_scale
 chmod -R 777 $tmpdir
+
+# check if this script has already been run
+isModuleLoaded=`lsmod | grep hid_sensor_hub`
+if [[ -n $isModuleLoaded ]]; then
+   # check if autorotate.sh is already running, otherwise start it
+   isAutorotateRunning=`ps -A | grep autorotate.sh`
+   if [ -z $isAutorotateRunning ]; then
+      autorotate.sh > /tmp/autorotate.log 2>&1 &
+   fi
+   # nothing more to do here
+   exit 0
+fi
+
 
 # self-explanatory...
 zenity --question --ok-label="Proceed" --cancel-label="Reboot" --text="If this is a <b>cold boot</b>, please click on <b>'Reboot'</b> in order to allow screen autorotate to work\n\nif you have just rebooted the system, click on 'Proceed'" 
